fd8567e801b64a1a306516277daaf4453b85282b
Anshumaan <anshumaan.1611@gmail.com>
2025-12-28 21:58:13 +0530

    Vercel deployment err resolved of unused route file

--- app/api/market/route.ts
+++ app/api/market/route.ts
@@ -4,25 +4,25 @@
 // const api = process.env.API;
 
 // export async function GET() {
-
-//     const res = await fetch(
-//         `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`,
-//         {
-//             headers: {
-//                 'x-cg-demo-api-key': api,
-//             },
-//             next: { revalidate: 60 }
-//         }
-//     );
-
-//     if (!res.ok) {
-//         // Error handling thoda clean kiya hai
-//         return NextResponse.json(
-//             { error: 'Failed to fetch data from CoinGecko' },
-//             { status: res.status }
-//         );
+//   const res = await fetch(
+//     `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`,
+//     {
+//       headers: {
+//         "x-cg-demo-api-key": api,
+//       },
+//       next: { revalidate: 60 },
 //     }
+//   );
 
-//     const data = await res.json();
-//     return NextResponse.json(data);
+//   if (!res.ok) {
+//     // Error handling thoda clean kiya hai
+//     return NextResponse.json(
+//       { error: "Failed to fetch data from CoinGecko" },
+//       { status: res.status }
+//     );
+//   }
+
+//   const data = await res.json();
+//   return NextResponse.json(data);
 // }
+export {};
