316d9cd26156d6f0529fc99bfb72e5a688907fd7
Anshumaan <anshumaan.1611@gmail.com>
2025-12-29 11:00:59 +0530

    Coingecko api setup and static images done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 export default function Home() {
   // const [coins,setCoins]= useState([]);
   // useEffect(()=>{
@@ -12,11 +12,36 @@ export default function Home() {
   // [])
 
   return (
-    <div>
-      <h1>Crypto Marketplace</h1>
+    <main className="main-container">
+      <section className="home-grid">
+        {/* <p>Coin overview</p> */}
+        <div id="coin-overview">
+          <div className="header">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="Bitcoin"
+              width={56}
+              height={56}
+            ></Image>
+            <div className="info">
+              <p>BitCoin/BTC</p>
+              <h1>$8900000 </h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending coins</p>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+
+      {/* <h1>Crypto Marketplace</h1> */}
       {/* {coins.map((coin)=>(
         <div key={coin.id}>{coin.name}:${coin.current_price}</div>
+
       ))} */}
-    </div>
+    </main>
   );
 }
